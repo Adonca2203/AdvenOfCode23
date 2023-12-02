1acd6f868be350a8886b0dd6e1e999119995c398
unknown <adonis.carvajal2203@gmail.com>
2023-12-01 21:10:20 -0500

    Rearranged folder heirarchy

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using AdventOfCode;
+using AdventOfCode.Solutions;
 
 Day1 day1 = new Day1();
 
 Console.WriteLine(day1.Problem1());
 
-Console.WriteLine(day1.Problem2() == 54824);
+Console.WriteLine(day1.Problem2());
